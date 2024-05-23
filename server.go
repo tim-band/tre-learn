cb6e97512a687b05b1c5623b983c666cfb1b41b7
Tim Band <t.band@nnnn.com>
2024-05-23 15:29:11 +0100

    html template served

--- server.go
+++ server.go
@@ -1,14 +1,24 @@
 package main
 
-import "github.com/gin-gonic/gin"
-//import "net/http"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+	r.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main website",
+		})
+	})
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run(":33330")
 }
